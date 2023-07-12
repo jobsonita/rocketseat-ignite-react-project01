940d22cc41be72b6c5fea9ac922a484b09e4b80c
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2023-07-12 00:23:07 +0000

    Lesson 2-6 - Component: Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,26 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/jobsonita.png" />
+          <div className={styles.authorInfo}>
+            <strong>Jobson Gilberto</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time dateTime="2023-07-11 20:15:30" title="11 de Julho às 20:15h">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href="">jane.design/doctorcare</a></p>
+        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
