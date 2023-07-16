f5dfbee88665a9b7b2d8bb6aa33dfdca104d0dcd
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2023-07-16 01:12:52 +0000

    Lesson 2-9 - Component: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img className={styles.avatar} src="https://github.com/jobsonita.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time dateTime="2023-07-15 20:06:45" title="15 de Julho às 20:06h">Cerca de 2h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👋👋</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
